94e948056eb8c4f542b69ec41037b854065b2b61
Strathox <benchude@gmail.com>
2018-01-26 18:46:55 +0100

    Setup start script amd heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -16,9 +17,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
+/**app.use((req, res, next) => {
   res.render('maintenance.hbs', {message: 'This site is undergoing maintenance!'});
-});
+});**/
 
 app.use(express.static(__dirname + '/public'));
 
@@ -56,7 +57,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-var port = 3000;
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`)
 });
